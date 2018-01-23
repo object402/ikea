7542a1189368dc4c3fdc9b607f7c48761896321b
meronaaa <33471288+meronaaa@users.noreply.github.com>
2018-01-24 04:03:56 +0900

    해결했다 자자
    ㅡㅡ ㅗ

--- js/main.js
+++ js/main.js
@@ -3,8 +3,21 @@ $(document).ready(function() {
 })
 
 function hiddenNav() {
+  //subnav 숨기기
   $('.gnb>ul>li>dl').hide();
+  //list의 사이즈 담는 변수
+  var listLength = $('.gnb>ul>li').length;
+
+  for(var i=0;i<listLength;i++){
+    //li>a 의 width를 담는 변수
+    var aWidth = $('.gnb>ul>li>a').eq(i).width();
+    //aWidth를 반올림하기위해 +1한 값을 width에
+    $('.gnb>ul>li').eq(i).css({
+      'width': aWidth+1
+    })
+  }
+  //li 를 클릭했을때 subnav가 펼쳐지도록
   $('.gnb>ul>li').click(function () {
-    $(this).children('dl').show();
+    $(this).children('dl').slideDown();
   });
 }
