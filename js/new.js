77d7879cbb3446d94bbe390709a9f477fbf6d8c8
999999sy <33650909+999999sy@users.noreply.github.com>
2018-01-25 19:04:45 +0900

    dd

--- js/new.js
+++ js/new.js
@@ -1,3 +1,27 @@
 $(document).ready(function(){
 
+    $('.p_goods_list li').mouseover(function(){
+
+      var img_index = $(this).index()+1;
+      console.log(img_index);
+      $(this).find('img').attr({"src":"./img/new/"+img_index+"1.jpg"});
+      // $(this).find('img').stop().animate({
+      //   'width':'240px'
+      // },'slow');
+    })
+
+    $('.p_goods_list li').mouseleave(function(){
+
+      var img_index = $(this).index()+1;
+      console.log(img_index);
+      $(this).find('img').attr({"src":"./img/new/"+img_index+"0.jpg"});
+      // $(this).find('img').stop().animate({
+      //   'width':'230px'
+      // },'slow');
+
+    })
+
+
+
+
 })
