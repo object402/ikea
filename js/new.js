fac1316f08e0c96d57f04db95c57acaff89814c6
999999sy <33650909+999999sy@users.noreply.github.com>
2018-02-14 17:00:47 +0900

    지랄도병

--- js/new.js
+++ js/new.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 
       var img_index = $(this).index()+1;
       console.log(img_index);
-      $(this).find('img').attr({"src":"img/new/"+img_index+"1.JPG"});
+      $(this).find('img').attr({"src":"img/new/"+img_index+"1.jpg"});
       // $(this).find('img').stop().animate({
       //   'width':'240px'
       // },'slow');
